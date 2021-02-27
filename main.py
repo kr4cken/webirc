514ac246981d67304ff353e8c5ebeb414f7765fd
kr4cken <37340982+kr4cken@users.noreply.github.com>
2021-02-27 20:02:24 +0300

    flask-socketio

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import flask
+from flask_socketio import SocketIO 
 
 app = flask.Flask(__name__)
+app.config["SECRET_KEY"] = "skjdbvksld8123"
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
     return "Hello world!"
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
