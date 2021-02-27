541e1166dbdb24752ccdda726094dea986f8d662
kr4cken <37340982+kr4cken@users.noreply.github.com>
2021-02-28 02:12:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico",mimetype="image/vnd.microsoft.icon")
+    return flask.send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico",mimetype="image/vnd.microsoft.icon")
 
 @socketio.on("message")
 def handleMessage(data):
