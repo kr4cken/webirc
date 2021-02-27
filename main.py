aa4ed0929d278475ecb6b968279c773aec8acebd
kr4cken <37340982+kr4cken@users.noreply.github.com>
2021-02-27 20:54:00 +0300

    socket.io

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 import os
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path="/static")
 app.config["SECRET_KEY"] = "skjdbvksld8123"
 socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    return flask.render_template("index.html")
+
+@socketio.on("message")
+def handleMessage(data):
+    emit("new_message", data, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, port=5004)
