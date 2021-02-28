91aa43606aa56fb46f3be1f9fe8eb1a43ec43714
kr4cken <37340982+kr4cken@users.noreply.github.com>
2021-02-28 20:50:25 +0300

    login page & bug fixes & changes

--- main.py
+++ main.py
@@ -2,21 +2,38 @@ import os
 import re
 import json
 import flask
+import pymongo
 from flask_socketio import SocketIO, send, emit
 
+# flask setup
 app = flask.Flask(__name__, static_url_path="/static")
 app.config["SECRET_KEY"] = "skjdbvksld8123"
 socketio = SocketIO(app)
 nicknames = []
 
+# mongo setup
+client = pymongo.MongoClient("mongodb+srv://emir:JVDzK7oYjzxtDrUE@cluster0.awjab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.webirc
+
+# webirc
 @app.route("/")
 def index():
     return flask.render_template("index.html")
 
+# login
+@app.route("/login", methods=["GET"])
+def get_login():
+    return flask.render_template("login.html")
+@app.route("/login", methods=["POST"])
+def post_login():
+    pass
+
+# favicon
 @app.route("/favicon.ico")
 def favicon():
     return flask.send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype="image/vnd.microsoft.icon")
 
+# socket.io
 @socketio.on("message")
 def handleMessage(data):
     emit("new_message", data, broadcast=True)
@@ -25,15 +42,15 @@ def handleMessage(data):
 def handleNickname(nickname):
     if re.search("[a-z0-9_-]{1,20}$", nickname):
         if nickname in nicknames:
-            emit("nickname", "NO")
+            emit("new_nickname", "NO")
             return
         else:
-            emit("nickname", "OK")
+            emit("new_nickname", "OK")
             nicknames.append(nickname)
             emit("new_user", json.dumps({"nickname": nickname}), broadcast=True)
             return
     else:
-        emit("nickname", "BAD")
+        emit("new_nickname", "BAD")
         return
 
 if __name__ == "__main__":
---
+++ static/js/irc.js
@@ -0,0 +1,149 @@
+var input = document.getElementById("chat-input");
+
+// press send when pressed the key enter
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("chat-send").click();
+    }
+});
+
+// setup
+var socket = io();
+var nickname = sessionStorage["nickname"] ? sessionStorage.getItem("nickname") : "";
+var color = sessionStorage["color"] ? sessionStorage.getItem("color") : "#FFFFFF";
+
+// just in case...
+var new_nickname = "";
+var new_color = "";
+
+// if new user
+if (nickname === "") {
+    document.getElementById("chat").innerHTML += "Welcome to WebIRC! You can set a nickname with /nick and start chatting right away<br>";
+    document.getElementById("chat").innerHTML += "You can get a list of commands with /help<br>";
+}
+
+// waiting function, for redirect
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function sendMessage() {
+    let message = input.value;
+
+    // if message is empty space
+    if (!message || message.length === 0 || /^\s*$/.test(message)) {
+        input.value = "";
+        return;
+    }
+
+    // check if message is a command
+    if (message.startsWith("/")) {
+        message = message.split(" ");
+
+        // set nickname
+        if (message[0] === "/nick" || message[0] === "/nickname") {
+            new_nickname = message[1];
+
+            // servera nickname değişikliğiyle ilgili istek gönder
+            socket.emit("nickname", new_nickname);
+        }
+
+        // redirect to login page
+        else if (message[0] === "/login") {
+            document.getElementById("chat").innerHTML += "Redirecting to login page...<br>";
+            sleep(1500).then(() => {
+                window.location = "/login"
+            });
+        }
+
+        // set user color
+        else if (message[0] === "/color") {
+            // if not logged in, return error
+            if (nickname === "") {
+                document.getElementById("chat").innerHTML += "<span class='text-danger'>Error: set a nickname first using /nick</span><br>";
+                input.value = "";
+                input.focus();
+                return;
+            }
+
+            new_color = message[1];
+            if (/^#[0-9A-F]{6}$/i.test(new_color)) {
+                color = new_color;
+                sessionStorage.setItem("color", color);
+                document.getElementById("chat").innerHTML += "<span style='color:" + color + "'>Color set! Type something to test it</span><br>";
+            } else {
+                document.getElementById("chat").innerHTML += "<span class='text-danger'>Error: Not a valid HEX color</span><br>";
+            }
+        }
+    }
+
+    // the message is just a message and not a command
+    else {
+        // if nickname is not set, return an error
+        if (nickname !== "") {
+            socket.emit("message", JSON.stringify({
+                "nickname": nickname,
+                "message": message,
+                "color": color
+            }));
+        }
+        else {
+            document.getElementById("chat").innerHTML += "<span class='text-danger'>Error: set a nickname first using /nick</span><br>";
+        }
+    }
+
+    // clear input
+    input.value = "";
+    input.focus();
+}
+
+// when a message is broadcasted
+socket.on("new_message", (data) => {
+    data = JSON.parse(data);
+
+    // if message mentions the client
+    if (data.message.split(" ").includes("@" + nickname) && nickname !== "") {
+        // if last character is whitespace, remove it
+        // it looks annoying as fuck
+        if (data.message.substring(data.message.length-1) == " ") {
+            data.message = data.message.substring(0, data.message.length-1);
+        }
+
+        // send the message with black text on yellow background
+        document.getElementById("chat").innerHTML += "<span style='color: " + data.color + "'>&lt;" + data.nickname + "&gt;</span> <span class='bg-warning text-dark'>" + data.message + "</span><br>";
+    }
+
+    // just... send.. the... message
+    else {
+        document.getElementById("chat").innerHTML += "<span style='color: " + data.color + "'>&lt;" + data.nickname + "&gt;</span> " + data.message + "<br>";
+    }
+
+    // auto scroll
+    document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+});
+
+//new user notification
+socket.on("new_user", (data) => {
+    data = JSON.parse(data);
+    document.getElementById("chat").innerHTML += "[" + data.nickname + " joined us]<br>";
+});
+
+// setting nickname
+socket.on("new_nickname", (response) => {
+    // valid nickname
+    if (response === "OK") {
+        nickname = new_nickname;
+        sessionStorage.setItem("nickname", new_nickname);
+    }
+    
+    // if the nickname is longer than 20 characters or includes a space
+    else if (response === "BAD") {
+        document.getElementById("chat").innerHTML += "<span class='text-danger'>Error: Nickname is not valid</span><br>";
+    }
+    
+    // if nickname is taken
+    else {
+        document.getElementById("chat").innerHTML += "<span class='text-danger'>Error: Nickname is already taken. Choose another nickname.</span><br>"
+    }
+});
