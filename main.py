6357fca8cccd198ec11d875c32e5bf481b9075c4
kr4cken <37340982+kr4cken@users.noreply.github.com>
2021-02-28 14:17:57 +0300

    Nickname ile ilgili geliştirmeler

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 app = flask.Flask(__name__, static_url_path="/static")
 app.config["SECRET_KEY"] = "skjdbvksld8123"
 socketio = SocketIO(app)
+nicknames = []
 
 @app.route("/")
 def index():
@@ -12,11 +13,21 @@ def index():
 
 @app.route("/favicon.ico")
 def favicon():
-    return flask.send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico",mimetype="image/vnd.microsoft.icon")
+    return flask.send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype="image/vnd.microsoft.icon")
 
 @socketio.on("message")
 def handleMessage(data):
     emit("new_message", data, broadcast=True)
+
+@socketio.on("nickname")
+def handleNickname(nickname):
+    if nickname in nicknames:
+        emit("nickname", "NO")
+        return
+    else:
+        emit("nickname", "OK")
+        nicknames.append(nickname)
+        return
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, port=5004)
